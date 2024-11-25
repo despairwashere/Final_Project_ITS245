e7b4275bc44746edba99a36ff4b294260628138e
despairwashere <bryanguy21@gmail.com>
2024-11-24 20:53:12 -0600

    Add the add buton functionality and delete button functionality

--- Healtcare Patient Application/PatientDemographicsForm.Designer.cs
+++ Healtcare Patient Application/PatientDemographicsForm.Designer.cs
@@ -33,7 +33,6 @@
             this.txtLastName = new System.Windows.Forms.TextBox();
             this.txtSSN = new System.Windows.Forms.TextBox();
             this.btnAdd = new System.Windows.Forms.Button();
-            this.btnModify = new System.Windows.Forms.Button();
             this.btnSave = new System.Windows.Forms.Button();
             this.btnDelete = new System.Windows.Forms.Button();
             this.btnBack = new System.Windows.Forms.Button();
@@ -77,6 +76,8 @@
             this.txtMedications = new System.Windows.Forms.TextBox();
             this.lblMedicalConditions = new System.Windows.Forms.Label();
             this.txtMedicalConditions = new System.Windows.Forms.TextBox();
+            this.lblPatientID = new System.Windows.Forms.Label();
+            this.txtPatientID = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // txtFirstName
@@ -116,15 +117,7 @@
             this.btnAdd.TabIndex = 4;
             this.btnAdd.Text = "Add";
             this.btnAdd.UseVisualStyleBackColor = true;
-            // 
-            // btnModify
-            // 
-            this.btnModify.Location = new System.Drawing.Point(871, 207);
-            this.btnModify.Name = "btnModify";
-            this.btnModify.Size = new System.Drawing.Size(75, 40);
-            this.btnModify.TabIndex = 5;
-            this.btnModify.Text = "Modify";
-            this.btnModify.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
             // 
             // btnSave
             // 
@@ -138,12 +131,13 @@
             // 
             // btnDelete
             // 
-            this.btnDelete.Location = new System.Drawing.Point(871, 306);
+            this.btnDelete.Location = new System.Drawing.Point(870, 249);
             this.btnDelete.Name = "btnDelete";
             this.btnDelete.Size = new System.Drawing.Size(75, 40);
             this.btnDelete.TabIndex = 8;
             this.btnDelete.Text = "Delete";
             this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
             // 
             // btnBack
             // 
@@ -609,12 +603,31 @@
             this.txtMedicalConditions.Size = new System.Drawing.Size(317, 26);
             this.txtMedicalConditions.TabIndex = 50;
             // 
+            // lblPatientID
+            // 
+            this.lblPatientID.AutoSize = true;
+            this.lblPatientID.Font = new System.Drawing.Font("Georgia", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPatientID.Location = new System.Drawing.Point(867, 48);
+            this.lblPatientID.Name = "lblPatientID";
+            this.lblPatientID.Size = new System.Drawing.Size(77, 18);
+            this.lblPatientID.TabIndex = 51;
+            this.lblPatientID.Text = "PatientID";
+            // 
+            // txtPatientID
+            // 
+            this.txtPatientID.Location = new System.Drawing.Point(807, 69);
+            this.txtPatientID.Name = "txtPatientID";
+            this.txtPatientID.Size = new System.Drawing.Size(204, 26);
+            this.txtPatientID.TabIndex = 52;
+            // 
             // PatientDemographicsForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(1802, 920);
+            this.Controls.Add(this.txtPatientID);
+            this.Controls.Add(this.lblPatientID);
             this.Controls.Add(this.txtMedicalConditions);
             this.Controls.Add(this.lblMedicalConditions);
             this.Controls.Add(this.txtMedications);
@@ -658,7 +671,6 @@
             this.Controls.Add(this.btnBack);
             this.Controls.Add(this.btnDelete);
             this.Controls.Add(this.btnSave);
-            this.Controls.Add(this.btnModify);
             this.Controls.Add(this.btnAdd);
             this.Controls.Add(this.txtSSN);
             this.Controls.Add(this.txtLastName);
@@ -678,7 +690,6 @@
         private System.Windows.Forms.TextBox txtLastName;
         private System.Windows.Forms.TextBox txtSSN;
         private System.Windows.Forms.Button btnAdd;
-        private System.Windows.Forms.Button btnModify;
         private System.Windows.Forms.Button btnSave;
         private System.Windows.Forms.Button btnDelete;
         private System.Windows.Forms.Button btnBack;
@@ -722,5 +733,7 @@
         private System.Windows.Forms.TextBox txtMedications;
         private System.Windows.Forms.Label lblMedicalConditions;
         private System.Windows.Forms.TextBox txtMedicalConditions;
+        private System.Windows.Forms.Label lblPatientID;
+        private System.Windows.Forms.TextBox txtPatientID;
     }
 }
--- Healtcare Patient Application/PatientDemographicsForm.cs
+++ Healtcare Patient Application/PatientDemographicsForm.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace Healtcare_Patient_Application
 {
@@ -67,6 +69,212 @@ namespace Healtcare_Patient_Application
             // Example:
             string query = "INSERT INTO patientdemographics (firstName, middleName, lastName, gender, dateOfBirth, ssn, phoneNumber, emailAddress, address, city, state, zipCode, emergencyContactName, emergencyContactRelationship, emergencyContactPhone, insuranceProvider, policyNumber, groupNumber, allergies, medications, medicalConditions) VALUES (@firstName, @middleName, @lastName, @gender, @dateOfBirth, @ssn, @phoneNumber, @emailAddress, @address, @city, @state, @zipCode, @emergencyContactName, @emergencyContactRelationship, @emergencyContactPhone, @insuranceProvider, @policyNumber, @groupNumber, @allergies, @medications, @medicalConditions)";
             // Use parameters to avoid SQL injection
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Collect the data from the form controls
+                string firstName = txtFirstName.Text;
+                string middleName = txtMiddleName.Text;
+                string lastName = txtLastName.Text;
+                string gender = cboGender.SelectedItem.ToString();
+                DateTime dateOfBirth = dtpDateOfBirth.Value;
+                string ssn = txtSSN.Text;
+                string phoneNumber = txtPhoneNumber.Text;
+                string email = txtEmailAddress.Text;
+                string address = txtAddress.Text;
+                string city = txtCity.Text;
+                string state = cboState.SelectedItem.ToString();
+                string zipCode = txtZipCode.Text;
+                string emergencyContactName = txtEmergencyContactName.Text;
+                string emergencyContactRelationship = txtEmergencyContactRelationship.Text;
+                string emergencyContactPhone = txtEmergencyContactPhone.Text;
+                string insuranceProvider = txtInsuranceProvider.Text;
+                string policyNumber = txtPolicyNumber.Text;
+                string groupNumber = txtGroupNumber.Text;
+                string allergies = txtAllergies.Text;
+                string medications = txtMedications.Text;
+                string medicalConditions = txtMedicalConditions.Text;
+
+                // Check if PatientID exists (assuming it's either a TextBox or a hidden field)
+                int patientId;
+                bool isEdit = int.TryParse(txtPatientID.Text, out patientId);  // Assuming the PatientID is being passed for edit
+
+                // Determine the SQL query based on whether it's an insert or update
+                string query;
+                SqlCommand cmd;
+
+                // Check if the PatientID exists for updating or inserting new record
+                if (isEdit)
+                {
+                    // Update existing patient record
+                    query = @"UPDATE patientdemographics 
+                      SET firstName = @firstName, middleName = @middleName, lastName = @lastName, gender = @gender,
+                          dateOfBirth = @dateOfBirth, ssn = @ssn, phoneNumber = @phoneNumber, emailAddress = @emailAddress,
+                          address = @address, city = @city, state = @state, zipCode = @zipCode,
+                          emergencyContactName = @emergencyContactName, emergencyContactRelationship = @emergencyContactRelationship,
+                          emergencyContactPhone = @emergencyContactPhone, insuranceProvider = @insuranceProvider,
+                          policyNumber = @policyNumber, groupNumber = @groupNumber, allergies = @allergies,
+                          medications = @medications, medicalConditions = @medicalConditions
+                      WHERE patientId = @patientId";
+                }
+                else
+                {
+                    // Insert new patient record
+                    query = @"INSERT INTO patientdemographics 
+                      (firstName, middleName, lastName, gender, dateOfBirth, ssn, phoneNumber, emailAddress, 
+                       address, city, state, zipCode, emergencyContactName, emergencyContactRelationship, 
+                       emergencyContactPhone, insuranceProvider, policyNumber, groupNumber, allergies, 
+                       medications, medicalConditions) 
+                      VALUES (@firstName, @middleName, @lastName, @gender, @dateOfBirth, @ssn, @phoneNumber, 
+                              @emailAddress, @address, @city, @state, @zipCode, @emergencyContactName, 
+                              @emergencyContactRelationship, @emergencyContactPhone, @insuranceProvider, 
+                              @policyNumber, @groupNumber, @allergies, @medications, @medicalConditions)";
+                }
+
+                // Create the SQL Command
+                using (SqlConnection conn = new SqlConnection("your_connection_string"))
+                {
+                    conn.Open();
+                    cmd = new SqlCommand(query, conn);
+
+                    // Add parameters to the query (same for insert and update)
+                    cmd.Parameters.AddWithValue("@firstName", firstName);
+                    cmd.Parameters.AddWithValue("@middleName", middleName);
+                    cmd.Parameters.AddWithValue("@lastName", lastName);
+                    cmd.Parameters.AddWithValue("@gender", gender);
+                    cmd.Parameters.AddWithValue("@dateOfBirth", dateOfBirth);
+                    cmd.Parameters.AddWithValue("@ssn", ssn);
+                    cmd.Parameters.AddWithValue("@phoneNumber", phoneNumber);
+                    cmd.Parameters.AddWithValue("@emailAddress", email);
+                    cmd.Parameters.AddWithValue("@address", address);
+                    cmd.Parameters.AddWithValue("@city", city);
+                    cmd.Parameters.AddWithValue("@state", state);
+                    cmd.Parameters.AddWithValue("@zipCode", zipCode);
+                    cmd.Parameters.AddWithValue("@emergencyContactName", emergencyContactName);
+                    cmd.Parameters.AddWithValue("@emergencyContactRelationship", emergencyContactRelationship);
+                    cmd.Parameters.AddWithValue("@emergencyContactPhone", emergencyContactPhone);
+                    cmd.Parameters.AddWithValue("@insuranceProvider", insuranceProvider);
+                    cmd.Parameters.AddWithValue("@policyNumber", policyNumber);
+                    cmd.Parameters.AddWithValue("@groupNumber", groupNumber);
+                    cmd.Parameters.AddWithValue("@allergies", allergies);
+                    cmd.Parameters.AddWithValue("@medications", medications);
+                    cmd.Parameters.AddWithValue("@medicalConditions", medicalConditions);
+
+                    if (isEdit)
+                    {
+                        // If updating an existing record, also add the PatientID to the parameters
+                        cmd.Parameters.AddWithValue("@patientId", patientId);
+                    }
+
+                    // Execute the query
+                    cmd.ExecuteNonQuery();
+                }
+
+                // Show a message indicating success
+                if (isEdit)
+                {
+                    MessageBox.Show("Patient record updated successfully.");
+                }
+                else
+                {
+                    MessageBox.Show("New patient record added successfully.");
+                }
+
+                // Optionally, clear fields after the operation
+                ClearFields();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}");
+            }
+        }
+
+        // Helper method to clear form fields (optional)
+        private void ClearFields()
+        {
+            txtFirstName.Clear();
+            txtMiddleName.Clear();
+            txtLastName.Clear();
+            cboGender.SelectedIndex = -1;
+            dtpDateOfBirth.Value = DateTime.Now;
+            txtSSN.Clear();
+            txtPhoneNumber.Clear();
+            txtEmailAddress.Clear();
+            txtAddress.Clear();
+            txtCity.Clear();
+            cboState.SelectedIndex = -1;
+            txtZipCode.Clear();
+            txtEmergencyContactName.Clear();
+            txtEmergencyContactRelationship.Clear();
+            txtEmergencyContactPhone.Clear();
+            txtInsuranceProvider.Clear();
+            txtPolicyNumber.Clear();
+            txtGroupNumber.Clear();
+            txtAllergies.Clear();
+            txtMedications.Clear();
+            txtMedicalConditions.Clear();
+            txtPatientID.Clear();  // Clear PatientID for new records
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Ensure that the PatientID is provided
+                int patientId;
+                if (int.TryParse(txtPatientID.Text, out patientId))
+                {
+                    // Confirm the deletion with the user
+                    DialogResult dialogResult = MessageBox.Show(
+                        "Are you sure you want to delete this patient record?",
+                        "Delete Record",
+                        MessageBoxButtons.YesNo,
+                        MessageBoxIcon.Warning);
+
+                    if (dialogResult == DialogResult.Yes)
+                    {
+                        // Set up the DELETE SQL query
+                        string query = "DELETE FROM patientdemographics WHERE patientId = @patientId";
+
+                        // Execute the query
+                        using (SqlConnection conn = new SqlConnection("your_connection_string"))
+                        {
+                            conn.Open();
+                            using (SqlCommand cmd = new SqlCommand(query, conn))
+                            {
+                                // Add the PatientID as a parameter to the SQL command
+                                cmd.Parameters.AddWithValue("@patientId", patientId);
+
+                                // Execute the DELETE query
+                                int rowsAffected = cmd.ExecuteNonQuery();
+
+                                // Check if any rows were affected (i.e., if the record was found and deleted)
+                                if (rowsAffected > 0)
+                                {
+                                    MessageBox.Show("Patient record deleted successfully.");
+                                }
+                                else
+                                {
+                                    MessageBox.Show("No record found for the provided PatientID.");
+                                }
+                            }
+                        }
+
+
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Please provide a valid PatientID to delete.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}");
+            }
         }
     }
 }
